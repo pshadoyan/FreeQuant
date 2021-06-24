ab9b030eb09b08e84efe2575c6bcf2ae513bf1bb
pshadoya <pshad001@odu.edu>
2021-06-24 13:47:57 -0400

    logic fix

--- sample/core.py
+++ sample/core.py
@@ -16,27 +16,29 @@ import backtrader.analyzers as btanalyzers
 import backtrader.feeds as btfeeds
 import backtrader.strategies as btstrats
 
-class maxRiskSizer(bt.Sizer):
-    '''
-    Returns the number of shares rounded down that can be purchased for the
-    max rish tolerance
-    '''
-    params = (('risk', 0.03),)
+import pandas
 
-    def __init__(self):
-        if self.p.risk > 1 or self.p.risk < 0:
-            raise ValueError('The risk parameter is a percentage which must be'
-                'entered as a float. e.g. 0.5')
+# class maxRiskSizer(bt.Sizer):
+#     '''
+#     Returns the number of shares rounded down that can be purchased for the
+#     max rish tolerance
+#     '''
+#     params = (('risk', 0.03),)
 
-    def _getsizing(self, comminfo, cash, data, isbuy):
-        if isbuy == True:
-            size = math.floor((cash * self.p.risk) / data[0])
-        else:
-            size = math.floor((cash * self.p.risk) / data[0]) * -1
-        return size
+#     def __init__(self):
+#         if self.p.risk > 1 or self.p.risk < 0:
+#             raise ValueError('The risk parameter is a percentage which must be'
+#                 'entered as a float. e.g. 0.5')
+
+#     def _getsizing(self, comminfo, cash, data, isbuy):
+#         if isbuy == True:
+#             size = math.floor((cash * self.p.risk) / data[0])
+#         else:
+#             size = math.floor((cash * self.p.risk) / data[0]) * -1
+#         return size
 
 class MyStrategy(bt.Strategy):
-    params = (('period', 15),('baseline', 30),('atrperiod', 14),('atrtp', 1),('atrsl', 1.5))
+    params = (('period', 15),('baseline', 30),('atrperiod', 14),('atrtp', 1),('atrsl', 1.5), ('trailperc', 0.03))
 
     def log(self, txt, dt=None):
         ''' Logging function fot this strategy'''
@@ -51,20 +53,15 @@ class MyStrategy(bt.Strategy):
         self.dataclose = self.datas[0].close
         # SimpleMovingAverage on main data
         # Equivalent to -> sma = btind.SMA(self.data, period=self.p.period)
-        self.sma = btind.MovingAverageSimple(self.dataclose, period=self.params.baseline)
-
+        self.sma = btind.MovingAverageSimple(period=self.params.baseline)
         #ATR
         self.atr = btind.AverageTrueRange(period = self.params.atrperiod)
        
-
         # CrossOver (1: up, -1: down) close / sma
         williams = btind.WilliamsR(period = self.params.period)
-        self.overline = btind.CrossUp(williams, -20, plot=True) 
+        self.overline = btind.CrossUp(williams, -20, plot=True)  
         self.belowline = btind.CrossDown(williams, -80, plot =True)
         # self.buysell = btind.CrossOver(williams, -50.0, plot =True)
-
-        # Sentinel to None: new ordersa allowed
-        self.order = None
 
     #strategy entry/exit rules and money management
     def next(self):
@@ -75,80 +72,56 @@ class MyStrategy(bt.Strategy):
 
         #Buy and Sell strategy conditiions
         self.buy_condition = abovesma and self.overline
-        self.sell_condition = (not abovesma) and self.belowline
-    
+        self.sell_condition = (not abovesma) and self.belowline     
+
+
         #not in a position
         if not self.position:
+
             if self.buy_condition:
 
-                self.entryB = self.dataclose
+                sdistb = self.atr[0] * self.params.atrsl
+                self.sstoplevelb = self.data.close[0] + sdistb
 
-               #ATR calculations
-                self.sdist = self.atr[0] * self.params.atrsl
-                self.sstop = self.dataclose - self.sdist
+                pdistb = self.atr[0] * self.params.atrtp
+                self.ptakelevelb = self.data.close[0] - pdistb
 
-                self.pdist = self.atr[0] * self.params.atrtp
-                self.ptake = self.dataclose + self.pdist
-                
                # self.log('BUY CREATE, %.2f' % self.data.close[0])
-                self.mainb = self.buy(exectype=bt.Order.Market)
+                self.mainb = self.buy(size=10, exectype=bt.Order.Market)
+                self.sell(size=5, exectype=bt.Order.Limit, price=self.ptakelevelb)
+                self.orderb = self.sell(size=10, exectype=bt.Order.Stop, price=self.sstoplevelb)
 
             if self.sell_condition:
+               
+                sdists = self.atr[0] * self.params.atrsl
+                self.sstoplevels = self.data.close[0] - sdists
 
-                self.entryS = self.dataclose
-                #ATR calculations
-                self.sdist = self.atr[0] * self.params.atrsl
-                self.sstop = self.dataclose + self.sdist
-
-                self.pdist = self.atr[0] * self.params.atrtp
-                self.ptake = self.dataclose - self.pdist
+                pdists = self.atr[0] * self.params.atrtp
+                self.ptakelevels = self.data.close[0] + pdists
 
                # self.log('SELL CREATE, %.2f' % self.data.close[0])
-                self.mains = self.sell(exectype=bt.Order.Market)
-
-        #in a position
+                self.mains = self.sell(size=10, exectype=bt.Order.Market)
+                self.buy(size=5, exectype=bt.Order.Limit, price=self.ptakelevels)
+                self.orders = self.buy(size=10, exectype=bt.Order.Limit, price=self.sstoplevels)
+               
         else:
-            #in a long?
+            
             if self.position.size > 0:
+                ptakelevelb = self.ptakelevelb
+                if(self.datas[0].close > ptakelevelb):
+                    self.cancel(self.orderb)
+                    self.sell(size=5, exectype=bt.Order.StopTrail, trailpercent=self.params.trailperc)
+            elif self.dataclose < 0:
+                ptakelevels = self.ptakelevels
+                if(self.datas[0].close < ptakelevels):
+                    self.cancel(self.orders)
+                    self.buy(size=5, exectype=bt.Order.StopTrail, trailpercent=self.params.trailperc)
 
-                #Trailing Stop for a buy order
-                if self.dataclose < self.sstop:
-                    self.sell(exectype=bt.Order.Stop, parent = self.mainb)
-                else:
-                   # Update only if greater than
-                   self.sstop = max(self.sstop, self.dataclose - self.sdist)
 
-                #take profit logic   
-                if self.dataclose < self.ptake:
-                    self.sell(size = self.position.size*0.5, exectype=bt.Order.Limit, parent = self.mainb)
-                    self.sstop = self.entryB + self.atr[0]
-                else:
-                    self.ptake = max(self.ptake, self.dataclose + self.pdist)
-
-            #in a short?
-            if self.position.size < 0:
-
-                #Trailing Stop for a sell order
-                if self.dataclose > self.sstop:
-                   self.buy(exectype=bt.Order.Stop, parent = self.mains)
-                else:
-                    # Update only if greater than
-                  self.sstop = max(self.sstop, self.dataclose + self.sdist)
-                
-                #take profit logic
-                if self.dataclose < self.ptake:
-                    self.buy(size = self.position.size * 0.5, exectype=bt.Order.Limit, parent = self.mains)
-                    self.sstop = self.entryS - self.atr[0]
-                else:
-                    self.ptake = max(self.ptake, self.dataclose + self.pdist)
-         
-
-    
 def runstrat():
-
     #Retrieve time series data
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, '../BTC-USD.csv')
+    datapath = os.path.join(modpath, 'BTC-USD.csv')
     data = btfeeds.YahooFinanceCSVData(dataname=datapath)
     #------------------------------------------------------   
     
@@ -165,23 +138,25 @@ def runstrat():
     c.addanalyzer(btanalyzers.SharpeRatio, _name='sharpe_ratio')
     c.addanalyzer(btanalyzers.DrawDown, _name='dd')
 
-    c.optstrategy(MyStrategy, period=range(5, 30), baseline=range(30, 70), atrperiod = 14, atrtp = range(1, 3), atrsl = range(1, 3))
+    c.optstrategy(MyStrategy, period=range(10, 30), baseline=range(50, 60), atrperiod = 14, atrtp = 1.5, atrsl = 1)
 
     c.addobserver(bt.observers.DrawDown)
     
     # c.addstrategy(MyStrategy)
     
-    c.addsizer(maxRiskSizer)
+    # c.addsizer(maxRiskSizer)
 
     optimized_runs = c.run()
 
     final_results_list = []
     for run in optimized_runs:
-        for strategy in run:
-            PnL = round(strategy.broker.get_value() - 10000,2)
-            sharpe = strategy.analyzers.sharpe_ratio.get_analysis()
-            dd = strategy.analyzers.dd.get_analysis()
-            final_results_list.append([strategy.params.period, strategy.params.baseline, strategy.params.atrsl, strategy.params.atrtp, dd['moneydown'] , PnL, sharpe['sharperatio']])
+        # c.plot()
+        for c in run:
+            
+            PnL = round(c.broker.get_value() - 10000,2)
+            sharpe = c.analyzers.sharpe_ratio.get_analysis()
+            dd = c.analyzers.dd.get_analysis()
+            final_results_list.append([c.params.period, c.params.baseline, c.params.atrsl, c.params.atrtp, dd['moneydown'] , PnL, sharpe['sharperatio']])
 
     sort_by_sharpe = sorted(final_results_list, key=lambda x: x[6], 
                              reverse=True)
@@ -191,7 +166,6 @@ def runstrat():
     for line in sort_by_sharpe[:10]:
         print(line)
 
-    # c.plot()
 
 if __name__ == '__main__':
     runstrat()
